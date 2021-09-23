466f62309d4fd1d04adc83311ddc24c1e04855e5
ParimalRaghavan <42382731+ParimalRaghavan@users.noreply.github.com>
2021-09-23 16:33:07 -0600

    Added buttons

--- Commands/JuryCommand.cs
+++ Commands/JuryCommand.cs
@@ -12,16 +12,25 @@ namespace DudesBot.Commands
 {
     public class JuryCommand : BaseCommandModule
     {
+        public bool inSession = false;
         public static Dictionary<DiscordMember, DateTimeOffset> activeUserDict = new();
 
         [Command("jury")]
-        public async Task Jury(CommandContext context)
+        public async Task Jury(CommandContext context, DiscordMember defendant, [RemainingText] string crime)
         {
-            foreach(var entry in activeUserDict)
+            if (inSession)
             {
-                await context.RespondAsync($"{entry.Key} : {entry.Value}");
+                context.Message.RespondAsync("The court is already in session!");
+                return;
             }
-            
+            inSession = true;
+            DiscordMember accuser = context.Member;
+            DiscordMessageBuilder msgBld = new DiscordMessageBuilder();
+            DiscordButtonComponent NotGuiltyButton = new DiscordButtonComponent(ButtonStyle.Success, "NotGuilty" + defendant.Id, "**NOT GUILTY**");
+            DiscordButtonComponent GuiltyButton = new DiscordButtonComponent(ButtonStyle.Danger, "Guilty" + defendant.Id, "**GUILTY**");
+            msgBld.AddComponents(new DiscordComponent[] { NotGuiltyButton, GuiltyButton });
+            msgBld.WithContent(Formatter.Mention(defendant) + "! You stand accused of **" + crime + "**! \n How do you plead?");
+            await context.Channel.SendMessageAsync(msgBld);
         }
 
         public static async Task ActiveUserCounter(DiscordClient client, MessageCreateEventArgs eventArgs)
